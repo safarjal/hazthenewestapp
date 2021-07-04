60d1aef3819239857d913ff9d8925d201927ff68
Ibraheem Zaman <ibraheem.zaman@mark43.com>
2021-07-04 13:46:12 +0500

    Use more IDs

--- src/main/kotlin/client.kt
+++ src/main/kotlin/client.kt
@@ -24,8 +24,13 @@ class Duration(
 
 object Ids {
     const val TABLE = "table"
-    const val START_TIME_INPUT = "start_time_input"
-    const val END_TIME_INPUT = "end_time_input"
+
+    object Row {
+        const val INPUT_START_TIME = "input_start_time"
+        const val INPUT_END_TIME = "input_end_time"
+        const val BUTTON_ADD = "button_add"
+        const val BUTTON_REMOVE = "button_remove"
+    }
 }
 
 val table get() = document.getElementById(Ids.TABLE) as HTMLTableElement
@@ -33,6 +38,8 @@ val table get() = document.getElementById(Ids.TABLE) as HTMLTableElement
 var rowId = 0
 fun getIncrementedRowId() = rowId++.toString()
 fun getRowById(rowId: String) = document.getElementById(rowId) as HTMLTableRowElement
+
+fun ParentNode.getChildById(id: String) = querySelector("#$id")
 
 fun main() {
     window.onload = {
@@ -61,12 +68,20 @@ fun Node.sayHello() {
 
 fun TagConsumer<HTMLElement>.inputRow(rowId: String) {
     td { +"Start" }
-    td { dateTimeLocalInput { id = Ids.START_TIME_INPUT } }
+    td { dateTimeLocalInput { id = Ids.Row.INPUT_START_TIME } }
     td { +"End" }
-    td { dateTimeLocalInput { id = Ids.END_TIME_INPUT } }
+    td { dateTimeLocalInput { id = Ids.Row.INPUT_END_TIME } }
     td {
-        button { +"Add"; onClickFunction = { addRow(rowId) } }
-        button { +"Remove"; onClickFunction = { removeRow() } }
+        button {
+            +"Add"
+            id = Ids.Row.BUTTON_ADD
+            onClickFunction = { addRow(rowId) }
+        }
+        button {
+            +"Remove"
+            id = Ids.Row.BUTTON_REMOVE
+            onClickFunction = { removeRow(rowId) }
+        }
     }
 }
 
@@ -78,21 +93,21 @@ fun addRow(rowId: String) {
     checkIfOnlyOneRow()
 }
 
-fun removeRow() {
-    table.removeChild(document.getElementById(rowId.toString())!!)
+fun removeRow(rowId: String) {
+    table.removeChild(document.getElementById(rowId)!!)
     checkIfOnlyOneRow()
 }
 
 fun checkIfOnlyOneRow() {
     val rows = table.rows
-    (rows[0]!!.lastChild!!.lastChild as HTMLButtonElement).disabled = rows.length == 1
+    (rows[0]!!.getChildById(Ids.Row.BUTTON_REMOVE) as HTMLButtonElement).disabled = rows.length == 1
 }
 
 fun parseEntries() {
     try {
         val entries = table.rows.asList().map { row ->
-            val startTime = (row.querySelector('#' + Ids.START_TIME_INPUT) as HTMLInputElement).value
-            val endTime = (row.querySelector('#' + Ids.END_TIME_INPUT) as HTMLInputElement).value
+            val startTime = (row.getChildById(Ids.Row.INPUT_START_TIME) as HTMLInputElement).value
+            val endTime = (row.getChildById(Ids.Row.INPUT_END_TIME) as HTMLInputElement).value
             if (startTime.isEmpty() || endTime.isEmpty()) {
                 window.alert("Please enter all the dates")
                 throw NullPointerException()
