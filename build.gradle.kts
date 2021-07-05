c40a8881ff272e15676556384a1235a33531f4e8
safarjal <safarjal22@gmail.com>
2021-07-05 18:23:34 +0500

    Project settings changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,17 @@ plugins {
     kotlin("js") version "1.5.10"
 }
 
-group = "me.ibraheem"
+group = "me.safar"
 version = "1.0-SNAPSHOT"
 
 repositories {
     jcenter()
     mavenCentral()
-    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-html:0.7.2")
-    //implementation(kotlin("stdlib-js"))
 }
 
 kotlin {
