102de6622e237f76dde6b4a18e56951f51b5cffc
Ibraheem Zaman <=>
2021-07-09 15:36:10 +0500

    Fix tests

--- src/test/kotlin/ClientTest.kt
+++ src/test/kotlin/ClientTest.kt
@@ -1,5 +1,7 @@
 import kotlinx.browser.document
 import org.w3c.dom.Element
+import org.w3c.dom.HTMLTableElement
+import org.w3c.dom.HTMLTableSectionElement
 import org.w3c.dom.get
 import kotlin.test.*
 
@@ -8,10 +10,11 @@ class ClientTest {
     fun testAddInputLayout_laysOutAllElements() {
         val container = document.createElement("div")
         container.addInputLayout()
-        val inputTable = assertNotNull(container.getChildById(Ids.INPUT_TABLE))
-        val rows = inputTable.childNodes
-        assertEquals(1, rows.length)
-        val row = assertIs<Element>(rows[0])
+        val inputDatesTable = assertIs<HTMLTableElement>(container.getChildById(Ids.INPUT_TABLE))
+        assertEquals(1, inputDatesTable.tBodies.length)
+        val inputDatesTableBody = assertIs< HTMLTableSectionElement>(inputDatesTable.tBodies[0])
+        assertEquals(1, inputDatesTableBody.rows.length)
+        val row = assertIs<Element>(inputDatesTableBody.rows[0])
         assertNotNull(row.getChildById(Ids.Row.INPUT_START_TIME))
         assertNotNull(row.getChildById(Ids.Row.INPUT_END_TIME))
         assertNotNull(row.getChildById(Ids.Row.BUTTON_REMOVE))
