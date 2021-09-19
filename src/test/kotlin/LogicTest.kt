f4be9b8017c8bdf6c4d8665f7795400469ddd8ab
safarjal <safarjal22@gmail.com>
2021-09-19 19:48:01 +0500

    tests not working, fix

--- src/test/kotlin/LogicTest.kt
+++ src/test/kotlin/LogicTest.kt
@@ -42,23 +42,23 @@ class LogicTest {
     }
     @Test
     fun testFiveSoortain(){
-        val mp = 21.0
-        val gp = 16.0
-        val dm = 11.0
-        val hz = 7.0
+        val mp:Long = 21L*MILLISECONDS_IN_A_DAY
+        val gp = 16L*MILLISECONDS_IN_A_DAY
+        val dm = 11L*MILLISECONDS_IN_A_DAY
+        val hz = 7L*MILLISECONDS_IN_A_DAY
         val output:FiveSoortainOutput = fiveSoortain(mp,gp,dm,hz)
         assertEquals(Soortain.B_3, output.soorat)
-        assertEquals(0.0, output.istihazaBefore)
-        assertEquals(7.0, output.haiz)
-        assertEquals(4.0, output.istihazaAfter)
+        assertEquals(0L, output.istihazaBefore)
+        assertEquals(7*MILLISECONDS_IN_A_DAY, output.haiz)
+        assertEquals(4*MILLISECONDS_IN_A_DAY, output.istihazaAfter)
         assertEquals(true, output.aadatTuhrChanges)
     }
     @Test
     fun testAddIndicesToFixedDurations(){
         val fixedDurations = mutableListOf(
-            FixedDuration(DurationType.DAM, timeInMilliseconds = (MILLISECONDS_IN_A_DAY*2).toLong()),
-            FixedDuration(DurationType.TUHR, timeInMilliseconds = (MILLISECONDS_IN_A_DAY*2).toLong()),
-            FixedDuration(DurationType.DAM, timeInMilliseconds = (MILLISECONDS_IN_A_DAY*2).toLong())
+            FixedDuration(DurationType.DAM, timeInMilliseconds = (MILLISECONDS_IN_A_DAY*2)),
+            FixedDuration(DurationType.TUHR, timeInMilliseconds = (MILLISECONDS_IN_A_DAY*2)),
+            FixedDuration(DurationType.DAM, timeInMilliseconds = (MILLISECONDS_IN_A_DAY*2))
         )
         addIndicesToFixedDurations(fixedDurations)
         assertEquals(0,fixedDurations[0].indices[0])
