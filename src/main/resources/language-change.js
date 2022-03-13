aec5d88118a37d4dddfc7e549fc5c6caef15ff84
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-03-13 05:55:39 -0600

    Refactor main page into two pages

--- src/main/resources/language-change.js
+++ src/main/resources/language-change.js
@@ -3,8 +3,38 @@ const onChangeFunction = (e) => {
   const englishElements = document.getElementsByClassName("english")
   const urduElements = document.getElementsByClassName("urdu")
 
-  console.log("englishElements", englishElements)
-  for (let i = 0; i < englishElements.length; i++) {englishElements[i].classList.toggle("lang-invisible", languageSelector.value == "urdu")}
-  for (let i = 0; i < urduElements.length; i++) {urduElements[i].classList.toggle("lang-invisible", languageSelector.value == "english")}
+  for (let i = 0; i < englishElements.length; i++) englishElements[i].classList.toggle("lang-invisible", languageSelector.value == "urdu")
+  for (let i = 0; i < urduElements.length; i++) urduElements[i].classList.toggle("lang-invisible", languageSelector.value == "english")
   document.body.classList.toggle("rtl", languageSelector.value == "urdu")
 }
+
+const onClickFunction = (lang) => {
+  const englishElements = document.getElementsByClassName("english-switch")
+  const urduElements = document.getElementsByClassName("urdu-switch")
+  const englishTab = document.getElementById("english-tab")
+  const urduTab = document.getElementById("urdu-tab")
+
+  englishTab.classList.toggle("selected", lang == "english")
+  urduTab.classList.toggle("selected", lang == "urdu")
+  for (let i = 0; i < englishElements.length; i++) englishElements[i].classList.toggle("invisible", lang == "urdu")
+  for (let i = 0; i < urduElements.length; i++) urduElements[i].classList.toggle("invisible", lang == "english")
+}
+
+function getUrlVars() {
+  var vars = {};
+  window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+      vars[key] = value;
+  });
+  return vars;
+}
+
+const onLoad = () => {
+  const lang = getUrlVars()["lang"]
+  if (lang == "en") {
+    const languageSelector = document.getElementById("language")
+    languageSelector.value = "english"
+    onChangeFunction(languageSelector)
+  }
+}
+
+window.onload = onLoad
