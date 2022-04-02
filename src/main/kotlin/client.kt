1aad13fa7cdc00b978149c3da02f911f69532579
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-04-02 11:33:20 -0600

    Fix mubtadia/mutada disabling

--- src/main/kotlin/client.kt
+++ src/main/kotlin/client.kt
@@ -204,7 +204,6 @@ fun handleLanguage() {
 
 fun languageChange() {
     val lang = languageSelector.value
-    console.log(languageElements)
     for (element in languageElements) element.classList.toggle("lang-invisible", !element.classList.contains(lang))
     document.body!!.classList.toggle("rtl", lang == "urdu")
     document.querySelectorAll("select")
@@ -629,7 +628,7 @@ private fun FlowContent.pregnancyTimeInput(inputContainerToCopyFrom: HTMLElement
 }
 
 private fun FlowContent.mutadaInputs(inputContainerToCopyFrom: HTMLElement?) {
-    div(classes = "row") {
+    div(classes = "row mutada") {
         makeLabel(Ids.AADAT_HAIZ_INPUT, StringsOfLanguages.ENGLISH.haizAadat, StringsOfLanguages.URDU.haizAadat)
         input {
             id = Ids.AADAT_HAIZ_INPUT
@@ -868,8 +867,6 @@ private fun TagConsumer<HTMLElement>.durationInputRow(lastWasDam: Boolean, disab
                 onChangeFunction = { event ->
                     val row = findRow(event)
                     val pregOct = (event.target as HTMLSelectElement).value in setOf("haml", "wiladat")
-                    println("preg")
-                    println(pregOct)
                     row.durationInput.value = if (pregOct) "0" else row.durationInput.value
                     row.durationInput.disabled = (event.target as HTMLSelectElement).value in setOf("haml", "wiladat")
                 }
@@ -1200,7 +1197,7 @@ private fun disableByClass(classSelector: String, classInvis: String, inputConta
 }
 private fun disableByMasla(inputContainer: HTMLElement) {
     disableByClass("is-nifas", "invisible", inputContainer, !inputContainer.isNifas)
-    disableByClass("mutada", "invisible", inputContainer, !inputContainer.isMubtadia)
+    disableByClass("mutada", "invisible", inputContainer, inputContainer.isMubtadia)
 }
 
 private fun parseEntries(inputContainer: HTMLElement) {
@@ -1234,7 +1231,6 @@ private fun parseEntries(inputContainer: HTMLElement) {
                 }
             }
             if(durations[0].type == DurationType.TUHR){ mawjodahtuhreditable = durations[0].timeInMilliseconds }
-            println(durations)
             for(dur in durations){
                 when (dur.type) {
                     DurationType.DAM -> {
