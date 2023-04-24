d6a13e7a985d4b7547c0c3b7731d2adb4127dd8a
qwibbler <88380694+qwibbler@users.noreply.github.com>
2023-04-24 08:56:53 -0600

    Replace date1 with var

--- src/main/kotlin/strings.kt
+++ src/main/kotlin/strings.kt
@@ -7,27 +7,27 @@ val urduStrings = Strings(
     "<b>واللہ تعالی اعلم بالصواب</b>\n\n",
     "فی الحال آپ کے پاکی کے دن ہیں اپنی عبادات جاری رکھیں۔\n\n",
     "فی الحال آپ کے حیض کے دن ہیں۔ خون جاری رہنے کی صورت میں عبادات سے رکیں۔\n\n",
-    "اگر خون اسی طرح جاری رہے یا فی الحال بند ہوجائے لیکن پندرہ دن کی کامل پاکی نہیں ملی کہ دوبارہ خون یا دھبہ آگیا تب پھر <b>date1 کو ضرور دوبارہ پوچھ لیں، اس لیے کہ مسئلہ کی صورت بدل جائے گی۔</b>\n\n",
-    "اگر date1 تک بھی خون جاری ہی رہے  تب بھی <b>date1 کو غسل کر کے اپنی عبادات شروع کر دیں۔</b>\n\n",
+    "اگر خون اسی طرح جاری رہے یا فی الحال بند ہوجائے لیکن پندرہ دن کی کامل پاکی نہیں ملی کہ دوبارہ خون یا دھبہ آگیا تب پھر <b>${PlcHolder.DT1} کو ضرور دوبارہ پوچھ لیں، اس لیے کہ مسئلہ کی صورت بدل جائے گی۔</b>\n\n",
+    "اگر ${PlcHolder.DT1} تک بھی خون جاری ہی رہے  تب بھی <b>${PlcHolder.DT1} کو غسل کر کے اپنی عبادات شروع کر دیں۔</b>\n\n",
     "حمل سے پہلے اس ترتیب سے خون آیا اور پاکی ملی:\n\n",
     "\n<b>حمل</b>\n",
-    "\n<b>ولادت date1</b>\n",
+    "\n<b>ولادت ${PlcHolder.DT1}</b>\n",
     "\n<b>ولادت کے بعد اس ترتیب سے خون آیااور پاکی ملی:</b>\n",
-    "\n<b>date1 کو اسقاط ہوا (غیر مستبین الخلقہ)</b>\n",
-    "اگر خون date1 سے پہلے بند ہو جاۓ تو غسل کر کے نمازیں شروع کر لیں، لیکن احتیاطا date1 کو بھی غسل کر لیجیے۔\n\n",
-    "اگر سائلہ شادی شدہ ہیں تو یہ مسئلہ بھی مدنظر رکھیں: \n \n\nاگر خون رک بھي جاۓ اور غسل کر کے نمازيں بھي شروع کر لي ہوں، تب بھي date1 سے پہلے صحبت کي اجازت نہيں۔\n\n",
-    "اگر خون اسی طرح جاری رہے یا فی الحال بند ہوجائے لیکن پندرہ دن کی کامل پاکی نہیں ملی کہ دوبارہ خون یا دھبہ آگیا تب پھر <b>date1 تک آپ کے یقینی پاکی کے دن ہونگے۔</b>\n\n",
+    "\n<b>${PlcHolder.DT1} کو اسقاط ہوا (غیر مستبین الخلقہ)</b>\n",
+    "اگر خون ${PlcHolder.DT1} سے پہلے بند ہو جاۓ تو غسل کر کے نمازیں شروع کر لیں، لیکن احتیاطا ${PlcHolder.DT1} کو بھی غسل کر لیجیے۔\n\n",
+    "اگر سائلہ شادی شدہ ہیں تو یہ مسئلہ بھی مدنظر رکھیں: \n \n\nاگر خون رک بھي جاۓ اور غسل کر کے نمازيں بھي شروع کر لي ہوں، تب بھي ${PlcHolder.DT1} سے پہلے صحبت کي اجازت نہيں۔\n\n",
+    "اگر خون اسی طرح جاری رہے یا فی الحال بند ہوجائے لیکن پندرہ دن کی کامل پاکی نہیں ملی کہ دوبارہ خون یا دھبہ آگیا تب پھر <b>${PlcHolder.DT1} تک آپ کے یقینی پاکی کے دن ہونگے۔</b>\n\n",
     "${UnicodeChars.SNOWFLAKE} <b>عادت: حیض: duration1، پاکی: duration2</b>\n\n",
-    "date1 تا date2 کل duration1 حیض کے ہیں۔\n\n",
-    "${UnicodeChars.RED_DIAMOND} date1 تا date2 کل duration1 حیض کے ہیں۔\n\n",
-    "${UnicodeChars.ORANGE_DIAMOND} date1 تا date2 کل duration1 یقینی پاکی (استحاضہ) کے ہیں۔\n\n",
-    "date1 تا date2 کل duration1 نفاس کے ہیں۔\n\n",
-    "${UnicodeChars.RED_DIAMOND} date1 تا date2 کل duration1 نفاس کے ہیں۔\n\n",
-    "\n\ndate1 سے date2 تک کل duration1 خون۔\n\n",
+    "${PlcHolder.DT1} تا date2 کل duration1 حیض کے ہیں۔\n\n",
+    "${UnicodeChars.RED_DIAMOND} ${PlcHolder.DT1} تا date2 کل duration1 حیض کے ہیں۔\n\n",
+    "${UnicodeChars.ORANGE_DIAMOND} ${PlcHolder.DT1} تا date2 کل duration1 یقینی پاکی (استحاضہ) کے ہیں۔\n\n",
+    "${PlcHolder.DT1} تا date2 کل duration1 نفاس کے ہیں۔\n\n",
+    "${UnicodeChars.RED_DIAMOND} ${PlcHolder.DT1} تا date2 کل duration1 نفاس کے ہیں۔\n\n",
+    "\n\n${PlcHolder.DT1} سے date2 تک کل duration1 خون۔\n\n",
     "${UnicodeChars.HAND_WRITING} <b>مسئلہ کا حل :</b>\n\n",
     "${UnicodeChars.BLACK_SQUARE} اس دوران میں جو نمازیں حیض سمجھ کر چھوڑیں،  ان کی قضاء ضروری ہے۔\n\n",
-    "${UnicodeChars.BLACK_SQUARE} date1 کو اگر غسل کر لیا تھا، تو غسل کے بعد والی نمازیں درست ہیں۔ اگر غسل نہیں کیا تھا، تو جب تک غسل نہیں کیا، اس کی نمازیں قضاء کریں۔\n\n",
-    "\n\ndate1 سے date2 تک کل duration1 خون جاری رھا (چونکہ آپ کو دو خون کے درمیان میں 15 دن کی کامل پاکی نہیں ملی ہے اسلیئے یوں سمجھا جائے گا کہ آپ کو مسلسل خون جاری ہی رہا ہے۔)\n\n",
+    "${UnicodeChars.BLACK_SQUARE} ${PlcHolder.DT1} کو اگر غسل کر لیا تھا، تو غسل کے بعد والی نمازیں درست ہیں۔ اگر غسل نہیں کیا تھا، تو جب تک غسل نہیں کیا، اس کی نمازیں قضاء کریں۔\n\n",
+    "\n\n${PlcHolder.DT1} سے date2 تک کل duration1 خون جاری رھا (چونکہ آپ کو دو خون کے درمیان میں 15 دن کی کامل پاکی نہیں ملی ہے اسلیئے یوں سمجھا جائے گا کہ آپ کو مسلسل خون جاری ہی رہا ہے۔)\n\n",
     "duration1 پاکی۔\n\n",
     "duration1 طہر فاسد۔\n\n",
     "duration1 استحاضہ + duration2 پاکی = duration3 طہر فاسد۔\n\n",
@@ -78,18 +78,18 @@ val urduStrings = Strings(
     "ولادت\n\n",
     "حمل\n\n",
     "شروع کے duration1 نفاس",
-    "اگر خون date1 یعنی 3 دن سے پہلے رک جاتا ہے،اور پھر مکمل 15 دن کی پاکی مل گیٔ تو یہ سب استحاضہ بن جاۓ گا۔\n\n",
-    "اگر خون date1 سے پہلے رک جاۓ اور پھر مکمل 15 دن کی پاکی مل گیٔ تو مسئلہ ضرور دوبارہ پوچھیے، اس لیے کے مسئلے کی صورت تبدیل ہو جاۓ گی۔\n\n",
-    "اگر خون date1 یعنی 40 دن کے بعد بھی جاری رہے، یا رک جاۓ لیکن 15 دن سے کم پاکی کے بعد دوبارہ شروع ہو جاۓ تو مسئلہ دوبارہ پوچھ لیجۓ۔\n\n",
-    "اگر خون date1 یعنی 10 دن کے بعد بھی جاری رہے، یا رک جاۓ لیکن 15 دن سے کم پاکی کے بعد دوبارہ شروع ہو جاۓ تو مسئلہ دوبارہ پوچھ لیجۓ۔\n\n",
+    "اگر خون ${PlcHolder.DT1} یعنی 3 دن سے پہلے رک جاتا ہے،اور پھر مکمل 15 دن کی پاکی مل گیٔ تو یہ سب استحاضہ بن جاۓ گا۔\n\n",
+    "اگر خون ${PlcHolder.DT1} سے پہلے رک جاۓ اور پھر مکمل 15 دن کی پاکی مل گیٔ تو مسئلہ ضرور دوبارہ پوچھیے، اس لیے کے مسئلے کی صورت تبدیل ہو جاۓ گی۔\n\n",
+    "اگر خون ${PlcHolder.DT1} یعنی 40 دن کے بعد بھی جاری رہے، یا رک جاۓ لیکن 15 دن سے کم پاکی کے بعد دوبارہ شروع ہو جاۓ تو مسئلہ دوبارہ پوچھ لیجۓ۔\n\n",
+    "اگر خون ${PlcHolder.DT1} یعنی 10 دن کے بعد بھی جاری رہے، یا رک جاۓ لیکن 15 دن سے کم پاکی کے بعد دوبارہ شروع ہو جاۓ تو مسئلہ دوبارہ پوچھ لیجۓ۔\n\n",
     "کیا غیر مستبین الخلقہ اسقاط کے مسئلے میں حمل والا طہر عادت بن سکتا ہے؟",
     "اختلافی مسائل",
     "کیا دور کے اخیر میں عادت سے زیادہ دن حیض بن سکتے ہیں؟",
-    "اگر date1 سے پہلے یا date1 کو  خون رک گیا اور 15 دن کی کامل پاکی مل گیٔ، تو یہ سب حیض شمار ہو گا اور اس دوران میں کی گیی عبادات کالعدم ہو جائں گی۔\n\n" +
-            "اور اگر date1 کے بعد بھی خون جاری رہا تو سب عبادات درست رہیں گی۔\n\n",
-    "اس وقت آپ کے لیے خون دیکھتے ہی نماز چھوڑنا درست نہیں ہے۔ آپ کو چاہیے کہ date1 تک عبادات جاری رکھیں۔\n\n" +
-            "آپ کے عادت کے دن date1 کو شروع ہو رہے ہیں۔ اگر خون تب تک جاری رہے تو date1 سے عابدات چھوڑ دیجیے۔\n\n",
-    "date1 تا date2 کل duration1 خون آیا۔ (یہ ایام قبلیہ کا مسئلہ ہے)۔\n\n",
+    "اگر ${PlcHolder.DT1} سے پہلے یا ${PlcHolder.DT1} کو  خون رک گیا اور 15 دن کی کامل پاکی مل گیٔ، تو یہ سب حیض شمار ہو گا اور اس دوران میں کی گیی عبادات کالعدم ہو جائں گی۔\n\n" +
+            "اور اگر ${PlcHolder.DT1} کے بعد بھی خون جاری رہا تو سب عبادات درست رہیں گی۔\n\n",
+    "اس وقت آپ کے لیے خون دیکھتے ہی نماز چھوڑنا درست نہیں ہے۔ آپ کو چاہیے کہ ${PlcHolder.DT1} تک عبادات جاری رکھیں۔\n\n" +
+            "آپ کے عادت کے دن ${PlcHolder.DT1} کو شروع ہو رہے ہیں۔ اگر خون تب تک جاری رہے تو ${PlcHolder.DT1} سے عابدات چھوڑ دیجیے۔\n\n",
+    "${PlcHolder.DT1} تا date2 کل duration1 خون آیا۔ (یہ ایام قبلیہ کا مسئلہ ہے)۔\n\n",
     "مبتداہ",
     "معتادہ",
     "مسئلے کی نوعیت",
@@ -124,35 +124,35 @@ val englishStrings = Strings(
     "You are in the days of your period right now, so, if bleeding continues you" +
             " cannot do any of your ritual worship.\n\n",
     "In case this bleeding continues without gap, or, it continues with gaps " +
-            "less than 15 days at a stretch, then <b>kindly do contact us again on date1 as " +
+            "less than 15 days at a stretch, then <b>kindly do contact us again on ${PlcHolder.DT1} as " +
             "the current ruling may change accordingly.</b>\n\n",
-    "If bleeding continues till date1, then you must <b>do ghusl on date1 and begin ibadaat.</b>\n\n",
+    "If bleeding continues till ${PlcHolder.DT1}, then you must <b>do ghusl on ${PlcHolder.DT1} and begin ibadaat.</b>\n\n",
     "The bleeding and purity (tuh’r) has been in the following sequence before pregnancy:\n\n",
     "\nPregnancy\n",
-    "\n<b>Date of delivery: date1</b>\n",
+    "\n<b>Date of delivery: ${PlcHolder.DT1}</b>\n",
     "\n<b>The bleeding and purity (tuh’r) has been in the following sequence after delivery:</b>\n",
-    "\n<b>Miscarriage of an undeveloped fetus confirmed on date1</b>\n",
-    "If the bleeding stops before date1 then take ghusl immediately and start saying" +
-            " your prayers but take ghusl on date1 too to be on the safe side.\n\n\n",
+    "\n<b>Miscarriage of an undeveloped fetus confirmed on ${PlcHolder.DT1}</b>\n",
+    "If the bleeding stops before ${PlcHolder.DT1} then take ghusl immediately and start saying" +
+            " your prayers but take ghusl on ${PlcHolder.DT1} too to be on the safe side.\n\n\n",
     "If the questioner is married, then she should keep this in mind as well: \n \n\nEven if " +
             "the bleeding stops and she performs ghusl and resumes salah, intimacy with husband " +
-            "remains prohibited till date1.\n\n",
+            "remains prohibited till ${PlcHolder.DT1}.\n\n",
     "In case this bleeding continues without complete 15 days of purity, " +
-            "then <b>until date1, are certainly the days of purity (tuhr)</b>\n\n",
+            "then <b>until ${PlcHolder.DT1}, are certainly the days of purity (tuhr)</b>\n\n",
     "${UnicodeChars.SNOWFLAKE} Habit: Haidh : duration1, Purity (Tuh’r): duration2 \n\n",
-    "date1 to date2, a total of duration1 of period.\n\n",
-    "${UnicodeChars.RED_DIAMOND} date1 to date2, a total of duration1 of period.\n\n",
-    "${UnicodeChars.ORANGE_DIAMOND} date1 to date2, a total of duration1 of definitive" +
+    "${PlcHolder.DT1} to date2, a total of duration1 of period.\n\n",
+    "${UnicodeChars.RED_DIAMOND} ${PlcHolder.DT1} to date2, a total of duration1 of period.\n\n",
+    "${UnicodeChars.ORANGE_DIAMOND} ${PlcHolder.DT1} to date2, a total of duration1 of definitive" +
             " purity (istihadha).\n\n",
-    "date1 to date2, a total of duration1 of post-natal bleeding.\n\n",
-    "${UnicodeChars.RED_DIAMOND} date1 to date2, a total of duration1 of post-natal bleeding.\n\n",
-    "\n\ndate1 to date2, a total of duration1 of blood.\n\n",
+    "${PlcHolder.DT1} to date2, a total of duration1 of post-natal bleeding.\n\n",
+    "${UnicodeChars.RED_DIAMOND} ${PlcHolder.DT1} to date2, a total of duration1 of post-natal bleeding.\n\n",
+    "\n\n${PlcHolder.DT1} to date2, a total of duration1 of blood.\n\n",
     "${UnicodeChars.HAND_WRITING} <b>Solution:</b>\n\n",
     "${UnicodeChars.BLACK_SQUARE} If you left any salah thinking of it as haidh and it turned " +
             "out to be istihadha, then make qada of them.\n\n",
-    "${UnicodeChars.BLACK_SQUARE} If you performed ghusl on date1, salah after " +
+    "${UnicodeChars.BLACK_SQUARE} If you performed ghusl on ${PlcHolder.DT1}, salah after " +
             "it are all valid. If you didn't, then make qada of all the salah until you took ghusl.\n\n",
-    "\n\nduration1 of continued bleeding from date1 to date2 (since you haven’t had 15 " +
+    "\n\nduration1 of continued bleeding from ${PlcHolder.DT1} to date2 (since you haven’t had 15 " +
             "or more blood-free days at a stretch in there)\n\n",
     "duration1 of purity (tuh’r)\n\n",
     "duration1 of invalid purity (tuh'r-e-faasid)\n\n",
@@ -208,27 +208,27 @@ val englishStrings = Strings(
     "Birth\n\n",
     "Pregnancy\n\n",
     "The first duration1 are nifas",
-    "If bleeding stops before date1 (3 days), and is followed by a 15 day" +
+    "If bleeding stops before ${PlcHolder.DT1} (3 days), and is followed by a 15 day" +
             " or more period of purity, then all of this will become istehadha.\n\n",
-    "If bleeding stops before date1 (3 days), then please ask again, " +
+    "If bleeding stops before ${PlcHolder.DT1} (3 days), then please ask again, " +
             "as the masla will change.\n\n",
-    "If bleeding continues after date1 (40 days), or stops, but starts again with gaps " +
+    "If bleeding continues after ${PlcHolder.DT1} (40 days), or stops, but starts again with gaps " +
             "of less than 15 days, then please ask the masla again.\n\n",
-    "If bleeding continues after date1 (10 days), or stops, but starts again " +
+    "If bleeding continues after ${PlcHolder.DT1} (10 days), or stops, but starts again " +
             "with gaps of less than 15 days, then please ask the masla again.\n\n",
     "Can the purity during pregnancy be used as a habit in case" +
             " of miscarriage of an unformed foetus?",
     "Differences of opinion",
     "Can more days than habit become haidh at the end of daur?",
-    "If bleeding stopped at or before date1, and there was an" +
+    "If bleeding stopped at or before ${PlcHolder.DT1}, and there was an" +
             " interval of at 15 days of complete purity after it, all of this will become haidh, and " +
             "ibadaat performed in this time will become null and void.\n\n" +
-            "If bleeding continues past date1, all ibadaat will remain valid.\n\n",
+            "If bleeding continues past ${PlcHolder.DT1}, all ibadaat will remain valid.\n\n",
     "It is not correct for you to stop ibadaat when bleeding starts. You should " +
-            "continue idadaat till date1, the start of your habit.\n\n" +
-            "Your habit days are beginning from date1. If bleeding continues till then, then you " +
-            "should stop praying from date1.\n\n",
-    "date1 to date2, a total of duration1 of bleeding. (This is a case of Ayyam-e-Qabliyya).\n\n",
+            "continue idadaat till ${PlcHolder.DT1}, the start of your habit.\n\n" +
+            "Your habit days are beginning from ${PlcHolder.DT1}. If bleeding continues till then, then you " +
+            "should stop praying from ${PlcHolder.DT1}.\n\n",
+    "${PlcHolder.DT1} to date2, a total of duration1 of bleeding. (This is a case of Ayyam-e-Qabliyya).\n\n",
     "Mubtadia",
     "Mu'tada",
     "Type of Masla",
