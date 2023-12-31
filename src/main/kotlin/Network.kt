c50e158780cddff2ec3c070a8816c3ab65d31628
qwibbler <hmemaz@gmail.com>
2023-12-31 12:45:51 -0600

    Remove extra slash

--- src/main/kotlin/Network.kt
+++ src/main/kotlin/Network.kt
@@ -6,14 +6,10 @@ import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.browser.localStorage
-import kotlinx.coroutines.await
-import kotlinx.serialization.decodeFromString
 import org.w3c.dom.HTMLElement
-import org.w3c.fetch.INCLUDE
-import org.w3c.fetch.RequestCredentials
 import kotlin.js.Json
 
-val HAZAPP_BACKEND = Url("https://hazapp.ztree.pk/")
+val HAZAPP_BACKEND = Url("https://hazapp.ztree.pk")
 //val HAZAPP_BACKEND = Url("http://localhost:3000/")
 const val AUTHORIZATION = "Authorization"
 val bearerToken get() = localStorage.getItem(AUTHORIZATION)
@@ -94,18 +90,17 @@ suspend fun sendData(toSend: SaveData): Json {
     return JSON.parse(response.body())
 }
 
-// TODO: Replace return type by response body JSON representing class
-suspend inline fun sendDataWithFetch(toSend: SaveData): Unit {
-    val response = fetch(
-        HAZAPP_BACKEND,
-        HttpMethod.Post,
-        toSend,
-        // I don't think this header makes sense as a request header
-        //Headers.build { append(HttpHeaders.AccessControlAllowOrigin, "*") },
-        credentials = RequestCredentials.INCLUDE,
-    )
-    return kotlinx.serialization.json.Json.decodeFromString(response.text().await())
-}
+//suspend inline fun sendDataWithFetch(toSend: SaveData): Unit {
+//    val response = fetch(
+//        HAZAPP_BACKEND,
+//        HttpMethod.Post,
+//        toSend,
+//        // I don't think this header makes sense as a request header
+//        //Headers.build { append(HttpHeaders.AccessControlAllowOrigin, "*") },
+//        credentials = RequestCredentials.INCLUDE,
+//    )
+//    return kotlinx.serialization.json.Json.decodeFromString(response.text().await())
+//}
 
 suspend fun loadData(id: String): Json {
     val response = client.get(HAZAPP_BACKEND) {
@@ -118,27 +113,27 @@ suspend fun loadData(id: String): Json {
     return JSON.parse(response.body())
 }
 
-fun reInputData(data: Json, inputsContainer: HTMLElement) {
-    with(inputsContainer) {
-        maslaSelect.value = data["typeOfMasla"].toString()
-        typeSelect.value = data["typeOfInput"].toString()
-//        entries = entries,
-//        answerEnglish = contentEnglish.textContent,
-//        answerUrdu = contentUrdu.textContent,
-    }
-//            title = titleText,
-//            question = questionText,
-//            aadatHaiz = aadatHaz.value,
-//            aadatTuhr = aadatTuhr.value,
-//            mawjoodahTuhr = mawjoodaTuhr.value,
-//            isMawjoodaFasid = isMawjoodaFasid,
-//            pregStartTime = if (isNifas) pregStartTime.value else null,
-//            birthTime = if (isNifas) pregEndTime.value else null,
-//            aadatNifas = if (isNifas) aadatNifas.value else null,
-//            mustabeenUlKhilqat = if (isNifas) isMustabeen else null,
-//            ghairMustabeenIkhtilaaf = ikhtilaf1,
-//            daurHaizIkhtilaf = ikhtilaf2,
-//            ayyameQabliyyaIkhtilaf = ikhtilaf3,
-//            mubtadiaIkhitilaf = ikhtilaf4,
-//            timeZone = if (isDateTime && !timezoneSelect.disabled) timezoneSelect.value else null,
-}
+//fun reInputData(data: Json, inputsContainer: HTMLElement) {
+//    with(inputsContainer) {
+//        maslaSelect.value = data["typeOfMasla"].toString()
+//        typeSelect.value = data["typeOfInput"].toString()
+////        entries = entries,
+////        answerEnglish = contentEnglish.textContent,
+////        answerUrdu = contentUrdu.textContent,
+//    }
+////            title = titleText,
+////            question = questionText,
+////            aadatHaiz = aadatHaz.value,
+////            aadatTuhr = aadatTuhr.value,
+////            mawjoodahTuhr = mawjoodaTuhr.value,
+////            isMawjoodaFasid = isMawjoodaFasid,
+////            pregStartTime = if (isNifas) pregStartTime.value else null,
+////            birthTime = if (isNifas) pregEndTime.value else null,
+////            aadatNifas = if (isNifas) aadatNifas.value else null,
+////            mustabeenUlKhilqat = if (isNifas) isMustabeen else null,
+////            ghairMustabeenIkhtilaaf = ikhtilaf1,
+////            daurHaizIkhtilaf = ikhtilaf2,
+////            ayyameQabliyyaIkhtilaf = ikhtilaf3,
+////            mubtadiaIkhitilaf = ikhtilaf4,
+////            timeZone = if (isDateTime && !timezoneSelect.disabled) timezoneSelect.value else null,
+//}
